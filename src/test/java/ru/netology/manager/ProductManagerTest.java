5d6ad922004f07b38ed10e463444a180aab77cfd
Kate <pink-angel@mail.ru>
2021-11-23 21:48:22 +0300

    Доработка

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -12,11 +12,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductManagerTest {
     ProductRepository repository = new ProductRepository();
     ProductManager manager = new ProductManager(repository);
-    Book book1 = new Book(1,"Name", 100, "A");
-    Book book2 = new Book(2,"B", 100, "B");
-    Book book3 = new Book(3,"Kk", 100, "A");
-    Smartphone smartphone1 = new Smartphone(3,"C", 100, "D");
-    Smartphone smartphone2 = new Smartphone(4,"D", 100, "s");
+    Book book1 = new Book(1, "Name", 100, "A");
+    Book book2 = new Book(2, "B", 100, "B");
+    Book book3 = new Book(3, "Kk", 100, "A");
+    Smartphone smartphone1 = new Smartphone(3, "C", 100, "D");
+    Smartphone smartphone2 = new Smartphone(4, "D", 100, "s");
 
     @BeforeEach
     public void setUp() {
@@ -31,34 +31,34 @@ class ProductManagerTest {
     void shouldCheckSearchBookByAuthor() {
         Product[] expected = {book1, book3};
         Product[] actual = manager.searchBy("A");
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void shouldCheckSearchSmartphoneByCompany() {
         Product[] expected = {smartphone2};
         Product[] actual = manager.searchBy("s");
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void shouldCheckNotFound() {
         Product[] expected = new Product[0];
         Product[] actual = manager.searchBy("G");
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void shouldCheckSearchBookByName() {
         Product[] expected = {book1};
         Product[] actual = manager.searchBy("Name");
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void shouldCheckSearchSmartphoneByName() {
         Product[] expected = {smartphone1};
         Product[] actual = manager.searchBy("C");
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
 }
--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.NotFoundException;
@@ -12,29 +13,26 @@ class ProductRepositoryTest {
     ProductRepository repository = new ProductRepository();
     ProductManager manager = new ProductManager(repository);
 
-    @Test
-    void shouldRemoveById() {
-        Book book1 = new Book(1,"Name", 100, "A");
-        Book book2 = new Book(2,"B", 100, "B");
-        Smartphone smartphone1 = new Smartphone(3,"C", 100, "D");
-        Smartphone smartphone2 = new Smartphone(4,"A", 100, "s");
+    Book book1 = new Book(1, "Name", 100, "A");
+    Book book2 = new Book(2, "B", 100, "B");
+    Smartphone smartphone1 = new Smartphone(3, "C", 100, "D");
+    Smartphone smartphone2 = new Smartphone(4, "A", 100, "s");
+
+    @BeforeEach
+    public void setUp() {
         repository.save(book1);
         repository.save(book2);
         repository.save(smartphone1);
         repository.save(smartphone2);
+    }
+
+    @Test
+    void shouldRemoveById() {
         repository.removeById(1);
     }
 
     @Test
     void shouldRemoveByIdNotFound() {
-        Book book1 = new Book(1,"Name", 100, "A");
-        Book book2 = new Book(2,"B", 100, "B");
-        Smartphone smartphone1 = new Smartphone(3,"C", 100, "D");
-        Smartphone smartphone2 = new Smartphone(4,"A", 100, "s");
-        repository.save(book1);
-        repository.save(book2);
-        repository.save(smartphone1);
-        repository.save(smartphone2);
         assertThrows(NotFoundException.class, () -> {
             repository.removeById(5);
         });
